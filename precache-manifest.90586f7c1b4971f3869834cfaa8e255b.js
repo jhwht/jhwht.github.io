b7711bb147fe760501f89f77e4c148f86b6d267a
jaakko <jhtakalo@gmail.com>
2020-09-20 15:15:00 +0300

    react test page

---
+++ precache-manifest.90586f7c1b4971f3869834cfaa8e255b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d362332f2d54116ff012dc56d14cb859",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e82a23a01f28ad66aa0",
+    "url": "/static/css/main.8d0919ed.chunk.css"
+  },
+  {
+    "revision": "e34da514574268de6f53",
+    "url": "/static/js/2.e39aa743.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e39aa743.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e82a23a01f28ad66aa0",
+    "url": "/static/js/main.fdc2fab4.chunk.js"
+  },
+  {
+    "revision": "015116872a8d081fa919",
+    "url": "/static/js/runtime-main.4e5ab081.js"
+  }
+]);
